ce1e83f31f68f98b8103c9cf13ac37c43bc7338c
Pratik <pratiksahane351@gmail,com>
2024-02-05 15:17:20 +0530

    Committed new changes

--- Login.java
+++ Login.java
@@ -1,43 +1,62 @@
 import java.util.Scanner;
-class Usercred{
+
+interface Authenticatable {
+    boolean validateCredentials(String username, int password);
+}
+
+class Employee implements Authenticatable {
     private String[] usernames = {"employee1", "employee2"};
-    private int[] password={1234,5678};
-    String name;
-    int password1;
-    public boolean validation(int code){
-        int temp=password[code];
-        if (usernames[code].equals(name) && temp==(password1)){
+    private int[] passwords = {1234, 5678};
 
-            return true;
+    @Override
+    public boolean validateCredentials(String username, int password) {
+        for (int i = 0; i < usernames.length; i++) {
+            if (usernames[i].equals(username) && passwords[i] == password) {
+                return true;
+            }
         }
-        else{
-            return false;
-        }
-
+        return false;
     }
 }
+
+class Manager implements Authenticatable {
+    private int managerPassword = 1234;
+    private String managerUsername = "employee3";
+
+    @Override
+    public boolean validateCredentials(String username, int password) {
+        return username.equals(managerUsername) && password == managerPassword;
+    }
+}
+
 public class Login {
-    
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner sc=new Scanner(System.in);
-        Usercred myobj=new Usercred();
-        System.out.println("Enter the employee code:(0,1)");
-        int code=sc.nextInt();
-        sc.nextLine(); 
-        System.out.println("Enter the user name:");
-        myobj.name=sc.nextLine();
+        System.out.println("Enter 'employee' if you are an employee and 'manager' if you are a manager:");
+        String userType = sc.nextLine();
+
+        Authenticatable authenticator;
+
+        if (userType.equals("employee")) {
+            authenticator = new Employee();
+        } else if (userType.equals("manager")) {
+            authenticator = new Manager();
+        } else {
+            System.out.println("Invalid user type.");
+            return;
+        }
+
+        System.out.println("Enter the username:");
+        String username = sc.nextLine();
         System.out.println("Enter the password:");
-        myobj.password1=sc.nextInt();
-        if (myobj.validation(code)){
-            System.out.println("Success full logged in");
+        int password = sc.nextInt();
+
+        if (authenticator.validateCredentials(username, password)) {
+            System.out.println("Successfully logged in.");
+        } else {
+            System.out.println("Invalid credentials.");
         }
-        else{
-            System.out.println("Invalid credentials");
-        }
-
-
-
-
     }
 }
